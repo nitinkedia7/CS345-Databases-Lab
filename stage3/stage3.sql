114a4d116ac41f995a2e6767a47cd81d9fb108b9
Nitin Kedia <nitinkedia7@gmail.com>
2019-02-08 11:16:38 +0530

    Refine stage3 in lab queries

--- stage3/stage3.sql
+++ stage3/stage3.sql
@@ -21,7 +21,7 @@ WHERE NOT EXISTS (SELECT * FROM enroll WHERE student_id=4 AND course_id=sx.cours
 -- Using difference
 SELECT DISTINCT name FROM student JOIN enroll AS sx ON id=student_id
 WHERE NOT EXISTS (
-    (SELECT course_id FROM enroll WHERE student_id = 2 AND course_id NOT IN
+    (SELECT course_id FROM enroll WHERE student_id = 6 AND course_id NOT IN
     (SELECT sp.course_id FROM enroll as sp WHERE sp.student_id=sx.student_id))
 );
 
@@ -41,10 +41,12 @@ SELECT DISTINCT name FROM student JOIN enroll ON id=student_id WHERE course_id I
 
 SELECT * FROM student
     WHERE 1 = (SELECT COUNT(student_id) FROM (
-        SELECT DISTINCT student_id, lecturer_id FROM enroll JOIN teach ON enroll.course_id = teach.course_id WHERE lecturer_id = 1 OR lecturer_id = 2)
+        SELECT DISTINCT student_id, lecturer_id FROM enroll JOIN teach ON enroll.course_id = teach.course_id WHERE lecturer_id = 1 OR lecturer_id = 2) A
         WHERE A.student_id = id
     );
 
+--check
+SELECT *  FROM enroll JOIN teach ON enroll.course_id = teach.course_id WHERE lecturer_id = 1 OR lecturer_id = 2 ORDER BY lecturer_id;
 
 SELECT name FROM student JOIN enroll ON id=student_id 
     WHERE student_id IN (
@@ -53,14 +55,14 @@ SELECT name FROM student JOIN enroll ON id=student_id
     )
     AND student_id NOT IN (
         SELECT student_id FROM enroll WHERE course_id IN (
-        SELECT course_id FROM teach WHERE lecturer_id=4
+        SELECT course_id FROM teach WHERE lecturer_id=2
         )
     )
 ) UNION
 SELECT name FROM student JOIN enroll ON id=student_id 
     WHERE student_id IN (
         SELECT student_id FROM enroll WHERE course_id IN (
-        SELECT course_id FROM teach WHERE lecturer_id=4
+        SELECT course_id FROM teach WHERE lecturer_id=2
     )
     AND student_id NOT IN (
         SELECT student_id FROM enroll WHERE course_id IN (
@@ -93,12 +95,14 @@ select distinct id,name from student where
 -- f
 SELECT dept_id, COUNT(*) FROM lecturer GROUP BY dept_id;
 -- f with dept names
-SELECT name, c FROM department JOIN 
-(SELECT dept_id, COUNT(*) as c FROM lecturer GROUP BY dept_id) A ON id=dept_id;
+-- SELECT name, c FROM department JOIN 
+-- (SELECT dept_id, COUNT(*) as c FROM lecturer GROUP BY dept_id) A ON id=dept_id;
+
+SELECT department.name, COUNT(*) FROM lecturer JOIN department ON dept_id=department.id GROUP BY dept_id
 
 --g
 SELECT * FROM student WHERE NOT EXISTS (
-    SELECT * FROM enroll JOIN teach ON enroll.course_id=teach.course_id WHERE student_id=id AND lecturer_id=4
+    SELECT * FROM enroll JOIN teach ON enroll.course_id=teach.course_id WHERE student_id=id AND lecturer_id=1
 );
 
 --h
@@ -111,14 +115,17 @@ GROUP BY dept_id, course_id;
 SELECT * FROM student JOIN enroll ON id=student_id ORDER BY dept_id, course_id;
 
 --i
-SELECT name, course_id, AVG(score) as a, dept_id FROM enroll JOIN course as SX ON course_id=id GROUP BY course_id 
-HAVING a >= ALL (SELECT AVG(score) FROM enroll JOIN (SELECT * FROM course WHERE dept_id=1) as SP ON course_id=id GROUP BY course_id);
+SELECT name, course_id, AVG(score) as max_avg_score, dept_id FROM enroll JOIN course as SX ON course_id=id GROUP BY course_id 
+HAVING max_avg_score >= ALL (SELECT AVG(score) FROM enroll JOIN (SELECT * FROM course WHERE dept_id=1) as SP ON course_id=id GROUP BY course_id);
+
+-- SELECT dept_id, name, MAX(avg_score) FROM (
+-- SELECT name, course_id, AVG(score) as avg_score, dept_id FROM enroll JOIN course ON course_id=id GROUP BY course_id) A GROUP BY dept_id;
 
 --j
 SELECT
-    CASE WHEN score < 4 THEN 'F'
-         WHEN score >= 4 AND score < 7 THEN 'B'
-         WHEN score >= 7 THEN 'A'
+    CASE WHEN score < 40 THEN 'F'
+         WHEN score >= 40 AND score < 70 THEN 'B'
+         WHEN score >= 70 THEN 'A'
     END Grade,
     COUNT(*) AS StudentCount
 FROM enroll WHERE course_id="MA225" GROUP BY 1;
